e94fb41fdcfbe314e554e4427f1885ba17714f65
Andrew Harris <ididrio@gmail.com>
2018-02-02 23:09:20 -0800

    added ability to set within nested objects easily

--- source/index.js
+++ source/index.js
@@ -3,9 +3,7 @@ import { dispatch, createStore, combineReducers } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-let MainStore, providerStore, initialStore = false;
-
-// ---- Start Of Redux Store Setup ---- //
+let storeReference, providerStore;
 
 const addToReduxStore = (object) => {
   return {
@@ -19,39 +17,39 @@ const mapStateToProps = store => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  addToReduxStore: object => dispatch(addToReduxStore(object)),
-  deleteFromReduxStore: object => dispatch(deleteFromReduxStore(object))
+  addToReduxStore: object => dispatch(addToReduxStore(object))
 });
 
 const storeReducer = (state = {}, action) => {
-  return Object.assign({}, state, action.payload);
+  if (typeof action.payload === 'object' && action.payload.hasOwnProperty('nestedObjectNames')) {
+    const arr = action.payload.nestedObjectNames;
+    let obj = state;
+    for (let i = 0; i < arr.length - 1; i++) {
+      obj = obj[arr[i]];
+    }
+    obj[arr[arr.length - 1]] = action.payload.value;
+    return Object.assign({}, state);
+  } else {
+    return Object.assign({}, state, action.payload);
+  }
 }
 
-const reducers = combineReducers({
+const reducer = combineReducers({
   reduxStore: storeReducer
 });
 
-// ----- End Of Redux Store Setup ----- //
-
-class AgentStore extends Component {
-  // Call Redux action creator with initial store passed in by client
-  componentWillMount() {
-    if (initialStore) this.props.props.addToReduxStore(this.props.props.props.initial);
-  }
-
-  render() {
-    // If an initial store was provided, wait for it to be set in Redux before rendering
-    if (initialStore && Object.keys(this.props.props.reduxStore).length === 0) return <div></div>;
-    else return this.props.props.props.children;
+class MainStore extends Component {
+  render() { 
+    return this.props.props.props.children; 
   }
 }
 
-class ReduxWrapper extends Component {
+class MainStoreWrapper extends Component {
 
   // Setting a reference to a React component so its props (Redux store and methods) can be accessed
   renderStore() {
-    MainStore = <AgentStore props={this.props} />;
-    return MainStore;
+    storeReference = <MainStore props={this.props} />;
+    return storeReference;
   }
 
   render() {
@@ -60,14 +58,14 @@ class ReduxWrapper extends Component {
 }
 
 // The Redux store and the Redux action creators are connected here
-const RW = connect(mapStateToProps, mapDispatchToProps)(ReduxWrapper);
+const ReactRedux = connect(mapStateToProps, mapDispatchToProps)(MainStoreWrapper);
 
 // The entire application is wrapped with Redux's Provider
 class ProviderWrapper extends Component {
   render() {
     return (
       <Provider store={providerStore} >
-        <RW props={this.props} />
+        <ReactRedux props={this.props} />
       </Provider>
     );
   }
@@ -76,34 +74,31 @@ class ProviderWrapper extends Component {
 const store = {
   Wrapper: (props) => {
     // Initial set up with any attributes used with Store component
-    if (props.hasOwnProperty('initial')) initialStore = true;
+    const initial = props.initial || {};
     if (props.devTools && props.devTools === true) {
-      providerStore = createStore(reducers, composeWithDevTools());
-    } else providerStore = createStore(reducers);
-
+      providerStore = createStore(reducer, { reduxStore: initial }, composeWithDevTools());
+    } else providerStore = createStore(reducer, { reduxStore: initial });
     return new ProviderWrapper(props);
   },
   set: (...args) => {
     if (args.length === 1 && typeof args[0] === 'object') {
       // If set with an object, just pass that to action creator  
-      MainStore.props.props.addToReduxStore(args[0]);
+      storeReference.props.props.addToReduxStore(args[0]);
     } else {
-      // If comma seperated strings, loops through them and 
-      // pass to action creator one by one
-      for (let i = 0; i < args.length; i = i + 2) {
-        if (i + 1 === args.length) MainStore.props.props.addToReduxStore({ [args[i]]: null });
-        else MainStore.props.props.addToReduxStore({ [args[i]]: args[i + 1] });
-      }
+      storeReference.props.props.addToReduxStore({
+        nestedObjectNames: args.slice(0, -1),
+        value: args[args.length - 1]
+      });
     }
   },
   get: (...keys) => {
     // Return the entire store if no arguments are provided
-    if (keys.length === 0) return MainStore.props.props.reduxStore;
+    if (keys.length === 0) return storeReference.props.props.reduxStore;
     else if (keys.length > 1) {
       const results = {};
-      keys.forEach(key => results[key] = MainStore.props.props.reduxStore[key]);
+      keys.forEach(key => results[key] = storeReference.props.props.reduxStore[key]);
       return results;
-    } else return MainStore.props.props.reduxStore[keys[0]];
+    } else return storeReference.props.props.reduxStore[keys[0]];
   }
 };
 
